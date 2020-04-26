36a0f557c3cd1cdee4ad13e2c24ded7385119193
SafaElmali <tsafaelmali@gmail.com>
2020-04-26 17:31:00 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView />
+        </>
+    );
+};
+
+export default App;
