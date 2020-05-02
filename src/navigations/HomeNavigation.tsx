e9ddded01b9ce4d4bb3095a40df2d1a7b4ef866d
SafaElmali <tsafaelmali@gmail.com>
2020-05-02 12:12:41 +0300

    header styles modified

--- src/navigations/HomeNavigation.tsx
+++ src/navigations/HomeNavigation.tsx
@@ -1,11 +1,12 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import { View, Image } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import TrendingScreen from '../screens/Trending/index';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Image } from 'react-native';
+import TrendingScreen from '../screens/Trending/index';
 import Category from '../screens/Category/index';
 
 const Stack = createStackNavigator();
@@ -24,18 +25,74 @@ const HomeStack = (props: any) => {
                     headerTitleAlign: 'left',
                     headerLeft: () => (
                         <TouchableOpacity onPress={onHandleOpenDrawer}>
-                            <Image
-                                source={require('../assets/images/drawer-icon.png')}
-                                style={{
-                                    width: 36,
-                                    height: 36,
-                                    marginHorizontal: 10
-                                }}
+                            <Icon
+                                name="view-headline"
+                                type="material-community"
+                                size={32}
+                                containerStyle={{ paddingLeft: 10 }}
+                                iconStyle={{ color: '#999999' }}
                             />
                         </TouchableOpacity>
                     ),
-                    headerStyle: {
-                        elevation: 0
+                    headerRight: () => (
+                        <View
+                            style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <TouchableOpacity onPress={onHandleOpenDrawer}>
+                                <Icon
+                                    name="search"
+                                    type="evilicon"
+                                    size={32}
+                                    iconStyle={{ color: '#999999' }}
+                                    containerStyle={{ marginHorizontal: 12 }}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={onHandleOpenDrawer}>
+                                <Icon
+                                    name="notifications"
+                                    type="material"
+                                    size={28}
+                                    iconStyle={{ color: '#999999' }}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={onHandleOpenDrawer}>
+                                <View>
+                                    <Image
+                                        source={{
+                                            uri:
+                                                'https://avatars1.githubusercontent.com/u/17435062?s=400&u=e7c2ce6f6e920acaa793ddd105e91fa2c624c713&v=4'
+                                        }}
+                                        style={{
+                                            width: 30,
+                                            height: 30,
+                                            borderRadius: 50,
+                                            marginHorizontal: 20
+                                        }}
+                                    />
+                                    <View
+                                        style={{
+                                            position: 'absolute',
+                                            bottom: -2,
+                                            right: 14,
+                                            width: 15,
+                                            height: 15,
+                                            backgroundColor: '#40FE34',
+                                            borderRadius: 50,
+                                            borderWidth: 2,
+                                            borderColor: '#fff'
+                                        }}
+                                    />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ),
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                        fontSize: 18
                     }
                 }}
             />
