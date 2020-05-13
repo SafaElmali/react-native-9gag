676471a02d5aefd6ebd27ddf513fed89500eed28
SafaElmali <tsafaelmali@gmail.com>
2020-05-13 17:20:16 +0300

    api key config modified

--- src/utils/networking/api/gif-service.js
+++ src/utils/networking/api/gif-service.js
@@ -1,23 +1,23 @@
 /* eslint-disable prettier/prettier */
-import { API_KEY } from 'react-native-dotenv';
+import config from '../config/config';
 import axios from '../config/axios-global';
 
 const getCategories = () => {
-    return axios().get(`gifs/categories?api_key=${API_KEY}`);
+    return axios().get(`gifs/categories?api_key=${config.API_KEY}`);
 };
 
 const getGifList = (categoryName) => {
     return axios().get(
-        `gifs/search?api_key=${API_KEY}&q=${categoryName}&limit=10`
+        `gifs/search?api_key=${config.API_KEY}&q=${categoryName}&limit=10`
     );
 };
 
 const getTrendSearchs = () => {
-    return axios().get(`trending/searches?api_key=${API_KEY}`);
+    return axios().get(`trending/searches?api_key=${config.API_KEY}`);
 };
 
 const getTrendGifs = (offset = 0) => {
-    return axios().get(`gifs/trending?api_key=${API_KEY}&limit=12&offset=${offset}`);
+    return axios().get(`gifs/trending?api_key=${config.API_KEY}&limit=12&offset=${offset}`);
 };
 
 export default { getCategories, getGifList, getTrendSearchs, getTrendGifs };
